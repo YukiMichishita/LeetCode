a24efae83f86c3c9d17adec438c979a3a420a09f
Yuki Michishita <yuki.michishita@mmmcorp.co.jp>
2024-01-14 21:16:05 +0900

    349-step2 refactoring

--- 349_intersection_of_two_arrays/step2_sort.py
+++ 349_intersection_of_two_arrays/step2_sort.py
@@ -5,16 +5,23 @@ class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         sorted_nums1 = sorted(nums1)
         sorted_nums2 = sorted(nums2)
-        intersection = set()
+        intersection = []
         i1 = 0
         i2 = 0
 
         while i1 < len(nums1) and i2 < len(nums2):
-            if sorted_nums1[i1] == sorted_nums2[i2]:
-                intersection.add(sorted_nums1[i1])
-            if sorted_nums1[i1] <= sorted_nums2[i2]:
+            if sorted_nums1[i1] < sorted_nums2[i2]:
                 i1 += 1
-            else:
+                continue
+            if sorted_nums2[i2] < sorted_nums1[i1]:
+                i2 += 1
+                continue
+
+            tmp_cursor = sorted_nums1[i1]
+            intersection.append(tmp_cursor)
+            while i1 < len(nums1) and tmp_cursor == sorted_nums1[i1]:
+                i1 += 1
+            while i2 < len(nums2) and tmp_cursor == sorted_nums2[i2]:
                 i2 += 1
 
-        return list(intersection)
+        return intersection
