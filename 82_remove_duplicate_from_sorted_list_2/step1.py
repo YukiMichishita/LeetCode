b1e3ebf9092dfe7f5fe34d626fd6b7705f6f5f84
Yuki Michishita <yuki.michishita@mmmcorp.co.jp>
2024-02-01 21:55:34 +0900

    nextのみを見るように変更

--- 82_remove_duplicate_from_sorted_list_2/step1.py
+++ 82_remove_duplicate_from_sorted_list_2/step1.py
@@ -7,23 +7,19 @@ class ListNode:
         self.next = next
 
 
-def get_val_safely(node: Optional[ListNode]):
-    if not node:
-        return None
-    return node.val
-
-
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        unique_linked_list = ListNode()
-        unique_list_head = unique_linked_list
+        sentinel = ListNode()
+        unique_linked_list = sentinel
         current = head
-        previous_val = None
         while current:
-            if current.val != previous_val and current.val != get_val_safely(current.next):
-                unique_linked_list.next = ListNode(current.val)
-                unique_linked_list = unique_linked_list.next
-            previous_val = current.val
+            if current.next and current.val == current.next.val:
+                while current.next and current.val == current.next.val:
+                    current = current.next
+                current = current.next
+                continue
+            unique_linked_list.next = ListNode(current.val)
+            unique_linked_list = unique_linked_list.next
             current = current.next
 
-        return unique_list_head.next
+        return sentinel.next
