3b0712f93e94c86e773bad375de841cab61aa398
Yuki Michishita <yuki.michishita@mmmcorp.co.jp>
2024-04-09 08:30:09 +0900

    refactoring step1

--- 695_max_area_of_island/step1.py
+++ 695_max_area_of_island/step1.py
@@ -9,22 +9,20 @@ class Solution:
         def calc_area(x: int, y: int) -> int:
             if grid[y][x] == 0:
                 return 0
-            area = 0
             visited = [[False] * width for _ in range(height)]
 
-            def search_land(x: int, y: int):
+            def search_land(x: int, y: int) -> int:
                 if x >= width or 0 > x or y >= height or 0 > y or visited[y][x] or grid[y][x] == 0:
-                    return
-                nonlocal area
-                area += 1
+                    return 0
+                area = 1
                 visited[y][x] = True
-                search_land(x + 1, y)
-                search_land(x - 1, y)
-                search_land(x, y + 1)
-                search_land(x, y - 1)
+                area += search_land(x + 1, y)
+                area += search_land(x - 1, y)
+                area += search_land(x, y + 1)
+                area += search_land(x, y - 1)
+                return area
 
-            search_land(x, y)
-            return area
+            return search_land(x, y)
 
         max_area = 0
         for y in range(height):
