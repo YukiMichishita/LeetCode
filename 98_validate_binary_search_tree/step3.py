24a7b10f9c6fec96336e4874c646aec6d06ec7f7
Yuki Michishita <yuki.michishita@mmmcorp.co.jp>
2024-04-24 09:10:26 +0900

    step3

---
+++ 98_validate_binary_search_tree/step3.py
@@ -0,0 +1,21 @@
+from typing import Optional
+from math import inf
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def check_nodes_recursively(node, lower_bound, upper_bound) -> bool: 
+            if not node:
+                return True
+            if not lower_bound < node.val < upper_bound:
+                return False
+            if not check_nodes_recursively(node.right, node.val, upper_bound):
+                return False
+            return check_nodes_recursively(node.left, lower_bound, node.val)
+        return check_nodes_recursively(root, -inf, inf)
+            
