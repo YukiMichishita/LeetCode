61b69c1a7f6c6c0ccb838a4f356e97388c026762
Yuki Michishita <yuki.michishita@mmmcorp.co.jp>
2024-05-13 22:53:55 +0900

    step1-3

--- 103_binary_tree_zigzag_level_order_traversal/step2.py
+++ 103_binary_tree_zigzag_level_order_traversal/step2.py
@@ -1,5 +1,6 @@
 from typing import Optional, List
 from collections import deque
+from collections import defaultdict
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -10,39 +11,58 @@ class TreeNode:
 # BFS
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        ordered_nodes = []
+        depth_to_ordered_values = defaultdict(deque)
         nodes_to_check = deque([(root, 0)])
         while nodes_to_check:
-            node, level = nodes_to_check.popleft()
+            node, depth = nodes_to_check.popleft()
             if not node:
                 continue
-            if len(ordered_nodes) <= level:
-                ordered_nodes.append(deque([node.val]))
+            if depth % 2 == 0:
+                depth_to_ordered_values[depth].append(node.val)
             else:
-                if level % 2 == 0:
-                    ordered_nodes[level].append(node.val)
-                else:
-                    ordered_nodes[level].appendleft(node.val)
-            nodes_to_check.append((node.left, level + 1))
-            nodes_to_check.append((node.right, level + 1))
-        return [list(node) for node in ordered_nodes]
+                depth_to_ordered_values[depth].appendleft(node.val)
+            nodes_to_check.append((node.left, depth + 1))
+            nodes_to_check.append((node.right, depth + 1))
+        return [list(values) for values in depth_to_ordered_values.values()]
 
 # DFS
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        ordered_values = []
-        def traverse_recursively(node, level):
+        depth_to_ordered_values = defaultdict(deque)
+        def traverse_recursively(node, depth):
             if not node:
                 return
-            if len(ordered_values) <= level:
-                ordered_values.append(deque([node.val]))
+            if depth % 2 == 0:
+                depth_to_ordered_values[depth].append(node.val)
             else:
-                if level % 2 == 0:
-                    ordered_values[level].append(node.val)
-                else:
-                    ordered_values[level].appendleft(node.val)
-            traverse_recursively(node.left, level + 1)
-            traverse_recursively(node.right, level + 1)
+                depth_to_ordered_values[depth].appendleft(node.val)
+            traverse_recursively(node.left, depth + 1)
+            traverse_recursively(node.right, depth + 1)
         traverse_recursively(root, 0)
-        return [list(node) for node in ordered_values]
+        return [list(values) for values in depth_to_ordered_values.values()]
+
+# ノードの探索自体をジグザグに行う
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        current_depth_nodes = [root]
+        next_depth_nodes = []
+        depth_to_ordered_values = defaultdict(list)
+        depth = 0
+        while current_depth_nodes or next_depth_nodes:
+            if not current_depth_nodes:
+                current_depth_nodes = next_depth_nodes
+                next_depth_nodes = []
+                depth += 1
+            node = current_depth_nodes.pop()
+            if not node:
+                continue
+            depth_to_ordered_values[depth].append(node.val)
+            if depth % 2 == 0:
+                next_depth_nodes.append(node.left)
+                next_depth_nodes.append(node.right)
+            else:
+                next_depth_nodes.append(node.right)
+                next_depth_nodes.append(node.left)
+
+        return list(depth_to_ordered_values.values())
 
