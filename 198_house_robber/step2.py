0bcba333cbbad7955edc5c9d58a1878dd1b6e9c0
Yuki Michishita <yuki.michishita@mmmcorp.co.jp>
2024-06-23 14:24:25 +0900

    PRコメント反映

--- 198_house_robber/step2.py
+++ 198_house_robber/step2.py
@@ -3,15 +3,15 @@ from typing import List
 # len(nums) <= 2の時とi = 1あたりをどう処理するかで人により好みがありそう
 
 # 例外的なケースに対する記述を最小限にした場合。
-# len(nums) == 1の時にdaily_robbable_amountsの長さが2になるのは直感に反するので、最初にearly returnした方がよいかもしれない。
+# len(nums) == 1の時にmax_robbable_amountsの長さが2になるのは直感に反するので、最初にearly returnした方がよいかもしれない。
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        daily_robbable_amounts = [nums[0], max(nums[:2])]
+        max_robbable_amounts = [nums[0], max(nums[:2])]
         for i in range(2, len(nums)):
-            include_i = daily_robbable_amounts[i - 2] + nums[i]
-            exclude_i = daily_robbable_amounts[i - 1]
-            daily_robbable_amounts.append(max(include_i, exclude_i))
-        return daily_robbable_amounts[len(nums) - 1]
+            include_i = max_robbable_amounts[i - 2] + nums[i]
+            exclude_i = max_robbable_amounts[i - 1]
+            max_robbable_amounts.append(max(include_i, exclude_i))
+        return max_robbable_amounts[-1]
 
 # 空間計算量O(1)
 class Solution:
